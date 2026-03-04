e6648d13fc6eda224eec970e6aba794ff2315c41
Sourabh Yelpale <sourabhyelpale29@gmail.com>
2026-03-04 17:20:07 +0530

    3 March 2026 - Feedback Doctype Created

---
+++ nexedu/ai_agent_hub/doctype/feedback_form_test/feedback_form_test.py
@@ -0,0 +1,29 @@
+# Copyright (c) 2026, Stride nex and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class FeedbackFormTest(Document):
+    def on_submit(self):
+
+        module_type = frappe.db.get_value("Feedback Form", self.feedback_form, "module_type")
+
+        doc = frappe.new_doc("Feedback Response")
+        doc.feedback_test = self.name
+        doc.feedback_form = self.feedback_form
+        doc.user = frappe.session.user
+        doc.module_type = module_type
+
+        for response in self.feedback_answer:
+
+            doc.append("feedback_answers", {
+                "question" : response.question,
+                "answer": response.answer
+            })
+
+        doc.insert(ignore_permissions=True)
+        doc.submit()
+
+        
